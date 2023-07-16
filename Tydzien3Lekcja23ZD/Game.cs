b67e5c01cef9d3b48d92b3bdd3b1a45edf6b3ed3
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-07-16 20:43:56 +0200

    Add project files.

---
+++ Tydzien3Lekcja23ZD/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tydzien3Lekcja23ZD
+{
+	internal class Game
+	{
+		private int _randomNumber { get; set; }
+		private int _counter { get; set; }
+		private bool _gameStatus { get; set; }
+
+		public Game(int range)
+		{
+			Random random = new Random();
+			this._counter = 0;
+			this._randomNumber = random.Next(range + 1);
+			this._gameStatus = false;
+		}
+
+		public bool GameStatus() { return this._gameStatus; }
+		public int HowManyAttemps() { return this._counter; }
+
+		public int Play(int number)
+		{
+			this._counter++;
+
+			if (number == this._randomNumber)
+			{
+				this._gameStatus = true;
+				return 0;
+			}
+			else if (number > this._randomNumber)
+			{
+				return 1;
+			}
+			else
+			{
+				return 2;
+			}
+
+		}
+	}
+}
---
+++ Tydzien3Lekcja23ZD/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Tydzien3Lekcja23ZD
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				var range = 100;
+
+				var game = new Game(range);
+				var number = 0;
+
+				Console.WriteLine("Witaj w grze \"Zgadnij liczbę!\"");
+
+				while (!game.GameStatus())
+				{
+					Console.Write($"TURA {game.HowManyAttemps()} | Podaj liczbę z przedziału [0 - {range}]: ");
+
+					while (!int.TryParse(Console.ReadLine(), out number) || number < 0 || number > range)
+					{
+						Console.Clear();
+						Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz!");
+						Console.Write($"TURA {game.HowManyAttemps()} | Podaj liczbę z przedziału [0 - {range}]: ");
+					}
+
+					Console.Clear();
+
+					switch (game.Play(number))
+					{
+						case 0:
+							Console.WriteLine($"Brawo, wylosowana liczba to {number}! Wygrałeś w {game.HowManyAttemps()} turach.");
+							break;
+						case 1:
+							Console.WriteLine("Podana liczba jest za duża, próbuj dalej!");
+							break;
+						case 2:
+							Console.WriteLine("Podana liczba jest za mała, próbuj dalej!");
+							break;
+						default:
+							throw new Exception();
+					}
+				}
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Występił nieoczekiwany błąd, aplikacja zostanie zamknięta!");
+			}
+			finally
+			{
+				Console.Write("Aby zamknąć aplikację naciśnij dowolny kawisz...");
+				Console.ReadKey();
+			}
+		}
+	}
+}
