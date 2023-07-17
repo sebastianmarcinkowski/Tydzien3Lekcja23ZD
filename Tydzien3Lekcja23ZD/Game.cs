9806b24dce0a0b2be79be071b0245137a78bc284
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-07-17 21:54:16 +0200

    Code refactoring.

--- Tydzien3Lekcja23ZD/Game.cs
+++ Tydzien3Lekcja23ZD/Game.cs
@@ -2,41 +2,36 @@
 
 namespace Tydzien3Lekcja23ZD
 {
-	internal class Game
-	{
-		private int _randomNumber { get; set; }
-		private int _counter { get; set; }
-		private bool _gameStatus { get; set; }
+    internal class Game
+    {
+        private int _randomNumber;
+        public int Counter { get; private set; }
+        public bool GameStatus { get; private set; }
 
-		public Game(int range)
-		{
-			Random random = new Random();
-			this._counter = 0;
-			this._randomNumber = random.Next(range + 1);
-			this._gameStatus = false;
-		}
+        public Game(int range)
+        {
+            Random random = new Random();
+            Counter = 0;
+            _randomNumber = random.Next(range + 1);
+            GameStatus = false;
+        }
+        public int Play(int number)
+        {
+            Counter++;
 
-		public bool GameStatus() { return this._gameStatus; }
-		public int HowManyAttemps() { return this._counter; }
-
-		public int Play(int number)
-		{
-			this._counter++;
-
-			if (number == this._randomNumber)
-			{
-				this._gameStatus = true;
-				return 0;
-			}
-			else if (number > this._randomNumber)
-			{
-				return 1;
-			}
-			else
-			{
-				return 2;
-			}
-
-		}
-	}
+            if (number == _randomNumber)
+            {
+                GameStatus = true;
+                return 0;
+            }
+            else if (number > _randomNumber)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+    }
 }
--- Tydzien3Lekcja23ZD/Program.cs
+++ Tydzien3Lekcja23ZD/Program.cs
@@ -2,57 +2,57 @@
 
 namespace Tydzien3Lekcja23ZD
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
-				var range = 100;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var range = 100;
+                var number = 0;
 
-				var game = new Game(range);
-				var number = 0;
+                var game = new Game(range);
 
-				Console.WriteLine("Witaj w grze \"Zgadnij liczbę!\"");
+                Console.WriteLine("Witaj w grze \"Zgadnij liczbę!\"");
 
-				while (!game.GameStatus())
-				{
-					Console.Write($"TURA {game.HowManyAttemps()} | Podaj liczbę z przedziału [0 - {range}]: ");
+                while (!game.GameStatus)
+                {
+                    Console.Write($"TURA {game.Counter} | Podaj liczbę z przedziału [0 - {range}]: ");
 
-					while (!int.TryParse(Console.ReadLine(), out number) || number < 0 || number > range)
-					{
-						Console.Clear();
-						Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz!");
-						Console.Write($"TURA {game.HowManyAttemps()} | Podaj liczbę z przedziału [0 - {range}]: ");
-					}
+                    while (!int.TryParse(Console.ReadLine(), out number) || number < 0 || number > range)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Podana wartość jest nieprawidłowa, spróbuj jeszcze raz!");
+                        Console.Write($"TURA {game.Counter} | Podaj liczbę z przedziału [0 - {range}]: ");
+                    }
 
-					Console.Clear();
+                    Console.Clear();
 
-					switch (game.Play(number))
-					{
-						case 0:
-							Console.WriteLine($"Brawo, wylosowana liczba to {number}! Wygrałeś w {game.HowManyAttemps()} turach.");
-							break;
-						case 1:
-							Console.WriteLine("Podana liczba jest za duża, próbuj dalej!");
-							break;
-						case 2:
-							Console.WriteLine("Podana liczba jest za mała, próbuj dalej!");
-							break;
-						default:
-							throw new Exception();
-					}
-				}
-			}
-			catch (Exception)
-			{
-				Console.WriteLine("Występił nieoczekiwany błąd, aplikacja zostanie zamknięta!");
-			}
-			finally
-			{
-				Console.Write("Aby zamknąć aplikację naciśnij dowolny kawisz...");
-				Console.ReadKey();
-			}
-		}
-	}
+                    switch (game.Play(number))
+                    {
+                        case 0:
+                            Console.WriteLine($"Brawo, wylosowana liczba to {number}! Wygrałeś w {game.Counter} turach.");
+                            break;
+                        case 1:
+                            Console.WriteLine("Podana liczba jest za duża, próbuj dalej!");
+                            break;
+                        case 2:
+                            Console.WriteLine("Podana liczba jest za mała, próbuj dalej!");
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Występił nieoczekiwany błąd, aplikacja zostanie zamknięta!");
+            }
+            finally
+            {
+                Console.Write("Aby zamknąć aplikację naciśnij dowolny kawisz...");
+                Console.ReadKey();
+            }
+        }
+    }
 }
